c62f3698403ae8edf1065d1d2b26cf7c517ffadb
吴可 <wuke6666@163.com>
2017-06-16 23:33:18 +0800

    添加12章-绘制曼德勃罗特集

---
+++ 12-MandelbrotSet/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace _12_MandelbrotSet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbMS = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbMS
+            // 
+            this.pcbMS.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pcbMS.Location = new System.Drawing.Point(0, 0);
+            this.pcbMS.Name = "pcbMS";
+            this.pcbMS.Size = new System.Drawing.Size(1301, 791);
+            this.pcbMS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pcbMS.TabIndex = 0;
+            this.pcbMS.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1301, 791);
+            this.Controls.Add(this.pcbMS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbMS;
+    }
+}
---
+++ 12-MandelbrotSet/Form2.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_MandelbrotSet
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //建立新的Image
+            pcbMS.Image = new Bitmap(pcbMS.Width, pcbMS.Height);
+
+            //计算各点的等级
+            int[,] temp = PointSet(23);
+
+            //生成新的Bitmap，并设置各点颜色
+            Bitmap bmpTemp = new Bitmap(pcbMS.Width, pcbMS.Height);
+            for (int i = 0; i < bmpTemp.Width; i++)
+            {
+                for (int j = 0; j < bmpTemp.Height; j++)
+                {
+                    bmpTemp.SetPixel(i, j, GetColor(temp[i, j]));
+                }
+            }
+
+            //赋值到PictureBox的Image上
+            pcbMS.Image = bmpTemp;
+        }
+        /// <summary>
+        /// 根据迭代次数分配颜色
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        Color GetColor(int i)
+        {
+            switch (i % 8)
+            {
+                case 0: return Color.Red;
+                case 1: return Color.Orange;
+                case 2: return Color.Yellow;
+                case 3: return Color.Green;
+                case 4: return Color.Cyan;
+                case 5: return Color.Blue;
+                case 6: return Color.Purple;
+                case 7: return Color.Black;
+                default: return Color.Black;
+            }
+        }
+
+        //复数结构
+        public struct Complex
+        {
+            public double Real; //实部
+            public double Imag; //虚部
+
+            /// <summary>
+            /// 建立一个复数 x+yi
+            /// </summary>
+            /// <param name="x">实部</param>
+            /// <param name="y">虚部</param>
+            public Complex(double x, double y)
+            {
+                Real = x;
+                Imag = y;
+            }
+
+            /// <summary>
+            /// 调整图形在窗口中的位置
+            /// </summary>
+            /// <param name="a">原实部位置</param>
+            /// <param name="b">原虚部位置</param>
+            /// <returns>调整后的位置</returns>
+            public static Complex Converse(double a, double b)
+            {
+                return new Complex(a / 380.0 - 2.4, b / 280.0 - 1.25);
+            }
+        }
+        /// <summary>
+        /// 点集求值
+        /// </summary>
+        /// <param name="maxiter">最大迭代次数</param>
+        /// <returns>求值后的集合</returns>
+        int[,] PointSet(int maxiter = 7)
+        {
+            int[,] MSet = new int[pcbMS.Width, pcbMS.Height];
+
+            var iter = 0;
+            var current = new Complex(0.0, 0.0);
+            var temp = new Complex(0.0, 0.0);
+
+            for (int x = 0; x < pcbMS.Width; x++)
+            {
+                for (int y = 0; y < pcbMS.Height; y++)
+                {
+                    iter = 0;
+                    current = Complex.Converse(x, y);
+                    temp = current;
+                    while (temp.Real * temp.Real + temp.Imag * temp.Imag <= 4 && iter < maxiter)
+                    {
+                        temp = new Complex(
+                            temp.Real * temp.Real - temp.Imag * temp.Imag + current.Real,
+                            2 * temp.Real * temp.Imag + current.Imag);
+                        iter = iter + 1;
+                    }
+
+                    MSet[x, y] = iter;
+                }
+            }
+
+            return MSet;
+        }
+    }
+}
