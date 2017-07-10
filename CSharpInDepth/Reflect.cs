21b9e2951b962fdd3823ec0f4d8237dfd8060f7a
wuke1988 <470041973@qq.com>
2017-07-10 15:25:01 +0800

    完善泛型 反射部分示例

--- CSharpInDepth/Reflect.cs
+++ CSharpInDepth/Reflect.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace CSharpInDepth
 {
+    /// <summary>
+    /// 泛型反射相关
+    /// </summary>
    static  class Reflect
     {
        public static void Test()
@@ -14,15 +17,17 @@ namespace CSharpInDepth
 
             Type type = Type.GetType(listTypeName);
 
+            //通过名称获取已构造泛型类型
             Type closedByName = Type.GetType(listTypeName+"[System.String]");
 
            
-
+            //通过typeof获取未绑定泛型类型
             Type closedByTypeof = typeof(List<>);
 
 
             //Type closedByMethod = type.MakeGenericType(typeof(string));
 
+            //通过MakeGenericType方法对未绑定泛型进行构造
             Type closedByMethod = closedByTypeof.MakeGenericType(typeof(string));
 
             closedByTypeof.GetMethods();
